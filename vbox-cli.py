70aa2c55c5cfdc8a4b4053cdf52f71c2a56285b3
mariachi9999 <57735911+mariachi9999@users.noreply.github.com>
2021-11-08 10:18:04 -0300

    Updating project structure

---
+++ vbox-cli.py
@@ -0,0 +1,36 @@
+import virtualbox
+from tools import IMachine
+
+
+vbox = virtualbox.VirtualBox()
+machines = vbox.machines
+# for machine in machines:
+#     print(machine.name)
+
+options = ["Create", "Delete", "Open", "Close"]
+
+
+print("CLI is running")
+print("Please, select and type below your wanted option...")
+for option in options:
+    print(option)
+command = input(">: ")
+
+
+if command == "Open":
+    print("Choose and type below on of the following existing machines...")
+    for machine in machines:
+        print(machine.name)
+    sel_machine = input(">: ")
+    print(sel_machine + " is opening")
+    session = virtualbox.Session()
+    machine = vbox.find_machine(sel_machine)
+    # progress = machine.launch_vm_process(session, "gui", "")
+    # For virtualbox API 6_1 and above (VirtualBox 6.1.2+), use the following:
+    progress = machine.launch_vm_process(session, "gui", [])
+    progress.wait_for_completion()
+    command = input(">: ")
+
+if command == "Close":
+    session.console.power_down()
+    command = input(">: ")
